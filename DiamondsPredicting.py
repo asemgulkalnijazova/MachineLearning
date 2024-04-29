2bf28f0f424679bdf346006f2a39ea45f0907645
asemgulkalnijazova <163097271+asemgulkalnijazova@users.noreply.github.com>
2024-04-29 17:08:24 +0600

    Тренировка и прогнозирование

--- DiamondsPredicting.py
+++ DiamondsPredicting.py
@@ -17,4 +17,20 @@ for i in range(3):
     new = le.fit_transform(df[categorical_features[i]])
     df[categorical_features[i]] = new
 
-print(df.head(10).to_string())
+#print(df.head(10).to_string())
+
+x = df[['carat', 'cut', 'color', 'clarity', 'depth', 'x', 'y', 'z']]
+y = df[['price']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 25, random_state = 101)
+
+regr = RandomForestRegressor(n_estimators = 10, max_depth = 10, random_state = 101) 
+regr.fit(x_train, y_train.values.ravel())
+
+predictions = regr.predict(x_test)
+
+result = x_test
+result['price'] = y_test
+result['predictions'] = predictions.tolist()
+
+print(result.to_string())
